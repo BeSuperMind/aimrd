1b9b163f91e91a03110bbff4ea9ac973648b052d
Madhav Mundhra <madhavmundhra221@gmail.com>
2024-11-18 20:21:14 +0530

    Resized for phone

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,27 @@ import MeditationTimer from "./components/MeditationTimer";
 import FeedbackForm from "./components/FeedbackForm";
 
 function App() {
-
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/main" element={<AppLayout />}>
-          <Route index element={<Main />} />
-        </Route>
-        <Route path="/meditate" element={<AppLayout />}>
-          <Route index element={<MeditatePage />} />
-        </Route>
-        <Route path="/timer" element={<AppLayout />}>
-          <Route index element={<MeditationTimer />} />
-        </Route>
-        <Route path="/feedback" element={<AppLayout />}>
-          <Route index element={<FeedbackForm />} />
-        </Route>
-      </Routes>
-    </>
-  )
+    <div className="bg-gray-100 min-h-screen flex justify-center items-center">
+      <div className="w-full max-w-sm bg-white shadow-lg rounded-lg overflow-hidden">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/main" element={<AppLayout />}>
+            <Route index element={<Main />} />
+          </Route>
+          <Route path="/meditate" element={<AppLayout />}>
+            <Route index element={<MeditatePage />} />
+          </Route>
+          <Route path="/timer" element={<AppLayout />}>
+            <Route index element={<MeditationTimer />} />
+          </Route>
+          <Route path="/feedback" element={<AppLayout />}>
+            <Route index element={<FeedbackForm />} />
+          </Route>
+        </Routes>
+      </div>
+    </div>
+  );
 }
 
 export default App;
